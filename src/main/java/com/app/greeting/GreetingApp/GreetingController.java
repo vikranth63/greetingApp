d3c9c698fb348eb63b36d394ee8f9c68ef210b3e
Vikranth <Velagala_Vikranth@cat.com>
2019-03-24 20:10:00 -0500

    push-3

--- src/main/java/com/app/greeting/GreetingApp/GreetingController.java
+++ src/main/java/com/app/greeting/GreetingApp/GreetingController.java
@@ -1,15 +1,22 @@
 package com.app.greeting.GreetingApp;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetimgController {
+public class GreetingController {
 	
 	@RequestMapping("/getGreeting")
 	public String getGreeting() {
 		
 		return "Hello AWS_CICD_Pipelines";
+	}
+	
+	@RequestMapping("/getDate")
+	public String getDate() {
+		return new Date().toString();
 	}
 
 }
